ce72a7d1de9bd0da9fa2ec8da1be728b725ffc65
Faan <rudfanno1@gmail.com>
2024-06-18 00:50:00 +0700

    add: re-usable Item model loade

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,28 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Home } from "../map/Home";
+import { Item } from "./shared/Item";
+
+const items = [
+  {
+    name: "StreetLight",
+    position: [10, 0, 0],
+    rotation: -45,
+  },
+  {
+    name: "Tank",
+    position: [2, 0, 0],
+    rotation: 1,
+  },
+];
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <Home/>
+      <Home />
+      {items.map((item, index) => (
+        <Item item={item} />
+      ))}
       <Environment preset="sunset" />
     </>
   );
---
+++ src/components/shared/Item.tsx
@@ -0,0 +1,26 @@
+import { useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
+import { SkeletonUtils } from "three-stdlib";
+
+interface ItemProps {
+  item: {
+    name: string;
+    position: number[];
+    rotation: number;
+  };
+}
+
+export const Item: React.FC<ItemProps> = ({ item }) => {
+  const { name, position, rotation } = item;
+  const { scene } = useGLTF(`/models/map_object/${name}.gltf`);
+  // Skinned meshes cannot be re-used in threejs without cloning them
+  const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+
+  return (
+    <primitive
+      object={clone}
+      position={position}
+      rotation={[0, ((rotation || 0) * Math.PI) / 2, 0]}
+    />
+  );
+};
