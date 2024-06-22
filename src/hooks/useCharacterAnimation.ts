6c857e001fd6dd2fee9ca25668ed4764532e695b
Faan <rudfanno1@gmail.com>
2024-06-22 21:35:33 +0700

    add: animation handler

---
+++ src/hooks/useCharacterAnimation.ts
@@ -0,0 +1,32 @@
+// useCharacterAnimation.ts
+import { useState } from "react";
+
+export enum AnimationState {
+    Idle = "idle",
+    Running = "running",
+    Jumping = "jumping"
+}
+
+export const useCharacterAnimation = () => {
+    const [animationState, setAnimationState] = useState<AnimationState>(AnimationState.Idle);
+
+  const updateAnimationState = (
+    forwardPressed: boolean,
+    backwardPressed: boolean,
+    leftPressed: boolean,
+    rightPressed: boolean,
+    jumpPressed: boolean,
+    isOnFloor: boolean,
+    jumpCooldown: boolean
+  ) => {
+    if (jumpPressed && !jumpCooldown && isOnFloor) {
+        setAnimationState(AnimationState.Jumping);
+      } else if (forwardPressed || backwardPressed || leftPressed || rightPressed) {
+        setAnimationState(AnimationState.Running);
+      } else {
+        setAnimationState(AnimationState.Idle);
+      }
+  };
+
+  return { animationState, updateAnimationState };
+};
